c304eb5c7c6949385430f5fd3b9f62ac9ca6a17a
qq8721443 <qq8721443@naver.com>
2026-03-17 01:47:17 +0900

    feat: configure project environment and path aliases

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from "node:path";
+import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
 });
